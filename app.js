cb85362dcbbc95a51a42618b0692720cf54710b6
camilamorand251 <magalicamila251@gmail.com>
2022-06-01 16:22:09 -0500

    [modificaciones]

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const public = path.join(__dirname, './public');
+app.use(express.static(public))
+
+app.get("/",function(req, res){
+    res.sendFile(path.join(__dirname,"./views/home.html"));
+})
+app.get("/login",function(req, res){
+    res.sendFile(path.join(__dirname,"./views/login.html"));
+})
+app.get("/register",function(req, res){
+    res.sendFile(path.join(__dirname,"./views/registro.html"));
+})
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("el servidor esta corriendo");
+}) 
